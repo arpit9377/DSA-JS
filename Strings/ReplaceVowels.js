8e1fa12303449ea93d042c2a3d254fc9bbfde91d
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-12-08 12:28:33 +0530

    DAY 3

---
+++ Strings/ReplaceVowels.js
@@ -0,0 +1,27 @@
+
+var reverseVowels = function(s) {
+    let arr = s.split('');
+
+
+    const isVowel = char => ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].includes(char);
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        while (left < right && !isVowel(arr[left])) {
+            left++;
+        }
+        while (left < right && !isVowel(arr[right])) {
+            right--;
+        }
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+    let result = arr.join('');
+    return result;
+};
+
+// Test the function
+console.log(reverseVowels("hello"));
